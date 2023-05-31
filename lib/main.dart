802d9bdd4d537cf269bc25b5985ea72f81f52b14
noah <noahvelasco299@gmail.com>
2023-05-31 00:32:59 -0500

    refactor main to modularize multi provider and to initialize secure storage & prompt database cleaner

--- lib/main.dart
+++ lib/main.dart
@@ -1,155 +1,179 @@
-// import 'package:ava_v2/view_models/export_view_models.dart';
-// import 'package:ava_v2/views/onboarding_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:provider/provider.dart';
+import 'package:ava_v2/view_models/export_view_models.dart';
+import 'package:ava_v2/views/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
-// import 'database/api_key_storage_helper.dart';
-// import 'providers/export_providers.dart';
-// import 'views/home_screen.dart';
+import 'database/api_key_storage_helper.dart';
+import 'database/prompt_storage_helper.dart';
+import 'providers/export_providers.dart';
+import 'views/home_screen.dart';
 
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-//   // lock the app into portrait mode
-//   SystemChrome.setPreferredOrientations([
-//     DeviceOrientation.portraitUp,
-//     DeviceOrientation.portraitDown,
-//   ]);
+  // lock the app into portrait mode
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
-//   //run the app
-//   runApp(Ava());
-// }
+  /*
+  Secure storage created at root of widget tree so it's available in 
+  onboarding_screen.dart & home_screen.dart for both the API keys.
+  */
+  final APIKeyStorageHelper apiKeyStorageHelper = APIKeyStorageHelper();
 
-// class Ava extends StatelessWidget {
-//   Ava({Key? key}) : super(key: key);
+  // Initialize the prompt storage database
+  final PromptStorageHelper promptStorageHelper = PromptStorageHelper();
 
-//   /*
-//   Secure storage created at root of widget tree so its available in 
-//   onboarding_screen.dart & home_screen.dart for both the api keys.
-//   */
-//   final APIKeyStorageHelper apiKeyStorageHelper = APIKeyStorageHelper();
+  // Insert preset data into the prompt storage database
+  await promptStorageHelper.insertData('Prompt 1', 'In simple terms, what is ');
+  await promptStorageHelper.insertData(
+      'Prompt 2', 'Explain the fast way how I can ');
 
-//   @override
-//   Widget build(BuildContext context) {
-//     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-//       statusBarColor: Colors.transparent,
-//     ));
+  debugPrint("YOU ARE IN MAIN 2");
 
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(
-//           create: (_) => ThemeProvider(),
-//         ),
-//         ChangeNotifierProvider(
-//           create: (_) => SettingsProvider(),
-//         ),
-//         ChangeNotifierProvider(
-//           create: (_) => LoadingIconGPTProvider(),
-//         ),
-//         ChangeNotifierProvider(
-//           create: (_) => LoadingIconELProvider(),
-//         ),
-//         ChangeNotifierProvider(
-//           create: (_) => GPTAPIViewModel(),
-//         ),
-//         ChangeNotifierProvider(
-//           create: (_) => ELAPIViewModel(),
-//         ),
-//         ChangeNotifierProvider(
-//           create: (_) => ChatHistoryProvider(),
-//         ),
-//       ],
-//       child: Consumer<ThemeProvider>(
-//         builder: (context, provider, child) {
-//           return MaterialApp(
-//             title: 'AVA',
-//             debugShowCheckedModeBanner: false,
-//             theme: ThemeData(
-//               // light theme settings
-//               useMaterial3: true,
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ThemeProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SettingsProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => LoadingIconGPTProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => LoadingIconELProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => GPTAPIViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ELAPIViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ChatHistoryProvider(),
+        ),
+      ],
+      //pass the api key database and the prompt database helpers to the main app
+      child: Ava(
+        apiKeyStorageHelper: apiKeyStorageHelper,
+        promptStorageHelper: promptStorageHelper,
+      ),
+    ),
+  );
+}
 
-//               /* light theme color settings */
-//               colorScheme: const ColorScheme.light(
-//                 background: Color(0xFFE7ECEF),
-//                 primary: Color(0xFFE7ECEF),
-//                 secondary: Color(0xFF252525),
-//               ),
-//               textTheme: GoogleFonts.montserratTextTheme(
-//                 const TextTheme(
-//                   bodyMedium: TextStyle(
-//                     color: Color(0xFF252525),
-//                     fontSize: 16.0,
-//                   ),
-//                   bodyLarge: TextStyle(
-//                     color: Color(0xFF252525),
-//                     fontSize: 25.0,
-//                     letterSpacing: 2.5,
-//                   ),
-//                 ),
-//               ),
-//               dialogTheme: DialogTheme(
-//                 backgroundColor: Colors.transparent,
-//                 surfaceTintColor: Colors.white,
-//                 iconColor: const Color(0xFFE7ECEF),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(15.0),
-//                   side: const BorderSide(
-//                     color: Color(0xFF252525),
-//                     width: 5.0,
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             darkTheme: ThemeData(
-//               // dark theme settings
-//               useMaterial3: true,
+class Ava extends StatelessWidget {
+  final APIKeyStorageHelper apiKeyStorageHelper;
+  final PromptStorageHelper promptStorageHelper;
 
-//               //changes the background color of the app thumbnail
-//               colorScheme: const ColorScheme.dark(
-//                 background: Color(0xFF252525),
-//                 primary: Color(0xFF252525),
-//                 secondary: Color(0xFFE7ECEF),
-//               ),
-//               textTheme: GoogleFonts.montserratTextTheme(
-//                 const TextTheme(
-//                   bodyMedium: TextStyle(
-//                     color: Color(0xFFE7ECEF),
-//                     fontSize: 16.0,
-//                   ),
-//                   bodyLarge: TextStyle(
-//                     color: Color(0xFFE7ECEF),
-//                     fontSize: 25.0,
-//                     letterSpacing: 2.5,
-//                   ),
-//                 ),
-//               ),
+  const Ava(
+      {Key? key,
+      required this.apiKeyStorageHelper,
+      required this.promptStorageHelper})
+      : super(key: key);
 
-//               dialogTheme: DialogTheme(
-//                 backgroundColor: Colors.transparent,
-//                 surfaceTintColor: Colors.white,
-//                 iconColor: const Color(0xFFE7ECEF),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(15.0),
-//                   side: const BorderSide(
-//                     color: Color(0xFFE7ECEF),
-//                     width: 5.0,
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             themeMode: provider.theme,
-//             home: Home(
-//               //pass the same secure storage object down to home now so we can print this in the settings section
-//               apiKeyStorageHelper: apiKeyStorageHelper,
-//             ),
-//             // home: OnBoardingPage(
-//             //   apiKeyStorageHelper: apiKeyStorageHelper,
-//             // ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+
+    return Consumer<ThemeProvider>(
+      builder: (context, provider, child) {
+        return MaterialApp(
+          title: 'AVA',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            // light theme settings
+            useMaterial3: true,
+
+            /* light theme color settings */
+            colorScheme: const ColorScheme.light(
+              background: Color(0xFFE7ECEF),
+              primary: Color(0xFFE7ECEF),
+              secondary: Color(0xFF252525),
+            ),
+            textTheme: GoogleFonts.montserratTextTheme(
+              const TextTheme(
+                bodyMedium: TextStyle(
+                  color: Color(0xFF252525),
+                  fontSize: 16.0,
+                ),
+                bodyLarge: TextStyle(
+                  color: Color(0xFF252525),
+                  fontSize: 25.0,
+                  letterSpacing: 2.5,
+                ),
+              ),
+            ),
+            dialogTheme: DialogTheme(
+              backgroundColor: Colors.transparent,
+              surfaceTintColor: Colors.white,
+              iconColor: const Color(0xFFE7ECEF),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                side: const BorderSide(
+                  color: Color(0xFF252525),
+                  width: 5.0,
+                ),
+              ),
+            ),
+          ),
+          darkTheme: ThemeData(
+            // dark theme settings
+            useMaterial3: true,
+
+            //changes the background color of the app thumbnail
+            colorScheme: const ColorScheme.dark(
+              background: Color(0xFF252525),
+              primary: Color(0xFF252525),
+              secondary: Color(0xFFE7ECEF),
+            ),
+            textTheme: GoogleFonts.montserratTextTheme(
+              const TextTheme(
+                bodyMedium: TextStyle(
+                  color: Color(0xFFE7ECEF),
+                  fontSize: 16.0,
+                ),
+                bodyLarge: TextStyle(
+                  color: Color(0xFFE7ECEF),
+                  fontSize: 25.0,
+                  letterSpacing: 2.5,
+                ),
+              ),
+            ),
+
+            dialogTheme: DialogTheme(
+              backgroundColor: Colors.transparent,
+              surfaceTintColor: Colors.white,
+              iconColor: const Color(0xFFE7ECEF),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                side: const BorderSide(
+                  color: Color(0xFFE7ECEF),
+                  width: 5.0,
+                ),
+              ),
+            ),
+          ),
+          themeMode: provider.theme,
+          home: Home(
+            //pass the same secure storage object down to home now so we can print this in the settings section
+            apiKeyStorageHelper: apiKeyStorageHelper,
+            promptStorageHelper: promptStorageHelper,
+          ),
+          // home: OnBoardingPage(
+          //   apiKeyStorageHelper: apiKeyStorageHelper,
+          // ),
+        );
+      },
+    );
+  }
+}
